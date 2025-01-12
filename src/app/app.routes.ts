ea1d15bfb2fdb9bb4fd461d3d421e006927c0f0f
Deepak Katariya <deepak.sorthiya@gmail.com>
2025-01-12 22:39:13 +0530

    routing module added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AttendanceComponent } from './attendance/attendance.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Page404Component } from './page404/page404.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'attendance', component: AttendanceComponent },
+  {
+    path: 'leaves',
+    loadChildren: () =>
+      import('./leaves/leaves.module').then((m) => m.LeavesModule),
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', component: Page404Component },
+];
---
+++ src/app/attendance/attendance.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attendance',
+  standalone: false,
+  
+  templateUrl: './attendance.component.html',
+  styleUrl: './attendance.component.css'
+})
+export class AttendanceComponent {
+
+}
