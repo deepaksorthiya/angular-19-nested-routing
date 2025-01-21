e2884530758c7734dd5fe5d6450bff45ceabd195
deepaksorthiya <deepak.sorthiya@gmail.com>
2025-01-21 06:00:06 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/deepaksorthiya/angular-19-nested-routing/commit/1c96ea790b2d3a116cd3bd908c6294e7da118740

--- chunk-ZA6VWDYZ.js
+++
@@ -1,1 +0,0 @@
-import{Ba as t,Pa as n,Qa as i,Ya as p}from"./chunk-4PMED3UX.js";var r=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-profile"]],decls:2,vars:0,template:function(e,a){e&1&&(n(0,"p"),p(1,"profile works!"),i())},encapsulation:2})};export{r as ProfileComponent};
